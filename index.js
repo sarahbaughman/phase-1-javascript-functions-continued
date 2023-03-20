cdd9d0211f168b317107b3b8a2a2d6899eed5bed
Sarah Baughman <sarahbaughman92@gmail.com>
2023-03-20 18:46:13 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork (workActivity= 'go to the office') {
+    return `This Monday, I will ${workActivity}.`
+}
+
+function wrapAdjective(string = '*') {
+    return function (paraMeter = 'special') {
+        return `You are ${string}${paraMeter}${string}!`
+    }
+}
+const encouragingPromptFunction = wrapAdjective("!!!")
+
